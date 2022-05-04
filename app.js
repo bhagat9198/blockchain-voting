0a96e606ad57fbcebc356304ecde75222cdfb949
bhagat singh <bhagatsinghsinghsingh1998@gmail.com>
2022-05-04 18:26:51 +0530

    signup implemenation done

--- app.js
+++ app.js
@@ -11,25 +11,26 @@ const voterRoutes = require('./routes/voter');
 const electionPartyRoutes = require('./routes/electionParty');
 const errorController = require('./controller/error');
 
-app.use(express.json())
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-log.info('req came  ::');
-
 app.use('/auth', authRoutes);
-app.use('/admin', authRoutes);
-app.use('/voter', authRoutes);
-app.use('/election-party', authRoutes);
-app.use('/', authRoutes);
+app.use("/admin", adminRoutes);
+app.use("/voter", voterRoutes);
+app.use("/election-party", electionPartyRoutes);
+// app.use('/', authRoutes);
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGODB_URI).then(async() => {
+mongoose.connect(MONGODB_URI).then(async () => {
   return app.listen(PORT)
 }).then(() => {
-  console.log();
+  console.log('Connected at PORT :: ', PORT);
+}).catch(error => {
+  console.log(error);
+  log.error('Error in connection.  ::', error.message);
 })
 
 
--- modal/user.js
+++ modal/user.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-
-})
+  name: {
+    type: String,
+  },
+  userType: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  voteCardId: {
+    type: String,
+  },
+  blogs: {
+    type: Array,
+  },
+  announcement: {
+    type: Array,
+  },
+  donation: {
+    type: Array,
+  },
+});
 
 module.exports = mongoose.model('User', UserSchema);
